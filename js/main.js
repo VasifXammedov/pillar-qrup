f2abb6aa8ee89fabf46ab2b5f3fd516a408c08a3
unknown <vasifvkh@code.edu.az>
2020-04-30 03:53:26 +0400

    Burda let.html -nen owr caruseli duzeltdik

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(window).scroll(function () { 
+    if($("html").scrollTop() >= 45){
+        $("#bashliq .navbar").addClass("scrolled");
+    }else{
+        $("#bashliq .navbar").removeClass("scrolled");
+    }
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items:6,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:1000,
+        autoplayHoverPause:true
+    });
+    $('.play').on('click',function(){
+        owl.trigger('play.owl.autoplay',[1000])
+    })
+    $('.stop').on('click',function(){
+        owl.trigger('stop.owl.autoplay')
+    })
+});
