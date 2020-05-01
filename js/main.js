ce05fb0d2e7a65eeccd192ef1774d352cf25905b
unknown <vasifvkh@code.edu.az>
2020-05-02 03:24:53 +0400

    Navstart ve js duzeltdik

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 $(window).scroll(function () { 
     if($("html").scrollTop() >= 45){
         $("#bashliq .navbar").addClass("scrolled");
+
+        $("#bashliq .navbar.navbarlogin").css("top", "0px");
+
+        $("#bashliq .navbar .nav-item .nav-link").css("color", "black");
+        
+        $("#bashliq .navbar-brand  img").attr("src", "./img/logo.png");
+
+        $("#bashliq .bashliq-nav  a i").css("color", "black");
+
     }else{
         $("#bashliq .navbar").removeClass("scrolled");
+
+        $("#bashliq .navbar .nav-item .nav-link").css("color", "white");
+        $("#bashliq .navbar-brand  img").attr("src", "./img/logo-light.png");
+
+        $("#bashliq .bashliq-nav  a i").css("color", "white");
+
+        $("#bashliq .navbar.navbarlogin").css("top", "45px");
+
+        $("#bashliq .navbar.navbarlogin .nav-item .nav-link").css("color", "black");
+        
+
+        $("#bashliq .bashliq-nav  a i.logini").css("color", "black");
+        $("#bashliq .navbar-brand  img").attr("src", "./img/logo.png");
+
+
     }
-
-
-    // $(".search-icon").click(function (e) { 
-    //     e.preventDefault();
-    //     $(".popup").addClass("popup-show");
-    //     $(".popup").css("display","block");
-    // }
-
     var owl = $('.owl-carousel');
     owl.owlCarousel({
         items:6,
@@ -28,3 +44,7 @@ $(window).scroll(function () {
         owl.trigger('stop.owl.autoplay')
     })
 });
+
+
+
+
